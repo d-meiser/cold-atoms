154d1bb1216ea673d2e08a5b7cd48b9c5ea2edef
Dominic Meiser <dmeiser79@gmail.com>
2017-01-23 22:06:00 -0700

    Add an optimization to push for the case of pure drift.

--- coldatoms/particles.py
+++ coldatoms/particles.py
@@ -3,9 +3,12 @@ import numpy as np
 
 def push(dt, x, v, accelerations=[]):
     """Drift-Kick-Drift push of particles."""
-    x += 0.5 * dt * v
-    acc = np.zeros_like(v)
-    for a in accelerations:
-        acc += a(x, v)
-    x += 0.5 * dt * v
+    if len(accelerations) == 0:
+        x += dt * v
+    else:
+        x += 0.5 * dt * v
+        acc = np.zeros_like(v)
+        for a in accelerations:
+            acc += a(x, v)
+        x += 0.5 * dt * v
 
