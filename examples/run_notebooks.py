d3fdbc59ef0a724eca009a3b1c7ea73be2c0e7dc
Dominic Meiser <dmeiser79@gmail.com>
2017-01-27 21:04:13 -0700

    Better file name for processed notebooks.

--- examples/run_notebooks.py
+++ examples/run_notebooks.py
@@ -1,6 +1,7 @@
 import sys
 import subprocess
 import tempfile
+import os.path
 
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
@@ -17,7 +18,9 @@ def notebook_run(notebook_file):
             err = 1
             raise
         finally:
-            with open('blah.ipynb', mode='wt') as fout:
+            base_extension = os.path.splitext(notebook_file)
+            outfile_name = base_extension[0] + '_out' + base_extension[-1]
+            with open(outfile_name, mode='wt') as fout:
                 nbformat.write(nb, fout)
         return err
 
