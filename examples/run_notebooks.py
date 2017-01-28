a979e2610984ae0cad0d4dd34bea3b877975228a
Dominic Meiser <dmeiser79@gmail.com>
2017-01-27 20:52:58 -0700

    Add a script to execute notebooks.

---
+++ examples/run_notebooks.py
@@ -0,0 +1,40 @@
+import sys
+import subprocess
+import tempfile
+
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+
+
+def notebook_run(notebook_file):
+    with open(notebook_file) as f:
+        nb = nbformat.read(f, as_version=4)
+        ep = ExecutePreprocessor(timeout=100)
+        err = 0
+        try:
+            ep.preprocess(nb, {})
+        except:
+            err = 1
+            raise
+        finally:
+            with open('blah.ipynb', mode='wt') as fout:
+                nbformat.write(nb, fout)
+        return err
+
+
+notebooks = ['./Free particles and ballistic expansion.ipynb']
+
+
+if __name__ == '__main__':
+    num_errors = 0
+    for n in notebooks:
+        print(n + ' ... ', end="")
+        sys.stdout.flush()
+        err = notebook_run(n)
+        num_errors += err
+        if err == 0:
+            print('PASS')
+        else:
+            print('FAIL')
+    sys.exit(num_errors)
+
