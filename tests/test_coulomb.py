38ae741b5dce24b5f779ead864e388211aeaff30
Dominic Meiser <dmeiser79@gmail.com>
2017-04-01 08:23:49 -0600

    Add test comparing coulomb forces with reference implementations.

--- tests/test_coulomb.py
+++ tests/test_coulomb.py
@@ -53,3 +53,25 @@ class test_coulomb(object):
     def test_can_evaluate_force_for_particles_in_same_position(self):
         self.ensemble.x[1] = np.zeros(3)
         f = self.coulomb_force.force(1.0e-1, self.ensemble)
+
+    def test_compare_with_reference_impl(self):
+        m, n = self.ensemble.x.shape
+        self.ensemble.x = np.random.rand(m, n)
+        f = self.coulomb_force.force(1.0e-2, self.ensemble)
+        self.coulomb_force.use_reference_implementations()
+        f_ref = self.coulomb_force.force(1.0e-2, self.ensemble)
+        normalization = np.linalg.norm(f_ref) + np.linalg.norm(f)
+        assert(np.linalg.norm(f - f_ref) < 1.0e-9 * normalization)
+
+    def test_compare_per_particle_charge_algs_with_reference_impl(self):
+        m, n = self.ensemble.x.shape
+        self.ensemble.x = np.random.rand(m, n)
+        q = np.random.rand(m)
+        self.ensemble.set_particle_property('charge', q)
+        self.ensemble.ensemble_properties = {}
+        f = self.coulomb_force.force(1.0e-2, self.ensemble)
+        self.coulomb_force.use_reference_implementations()
+        f_ref = self.coulomb_force.force(1.0e-2, self.ensemble)
+        normalization = np.linalg.norm(f_ref) + np.linalg.norm(f)
+        assert(np.linalg.norm(f - f_ref) < 1.0e-9 * normalization)
+
