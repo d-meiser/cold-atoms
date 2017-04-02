e7b410704d574b8680573295441f3bde0e41989d
Dominic Meiser <dominic@sketchup.com>
2017-04-02 09:32:06 -0600

    Fix indentation of forces.* files.

--- src/coldatoms_lib/forces.h
+++ src/coldatoms_lib/forces.h
@@ -2,10 +2,10 @@
 #define COLDATOMS_FORCES_H
 
 void coulomb_force(const double *positions, double charge,
-    int num_ptcls, double delta, double k, double *forces);
+	int num_ptcls, double delta, double k, double *forces);
 
 void coulomb_force_per_particle_charges(
-    const double *positions, const double *charge,
-    int num_ptcls, double delta, double k, double *forces);
+	const double *positions, const double *charge,
+	int num_ptcls, double delta, double k, double *forces);
 
 #endif
