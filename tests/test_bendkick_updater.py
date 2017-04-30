bf99eeba0d307cdd43bca2a2408795a11c6dac23
Dominic Meiser <dmeiser79@gmail.com>
2017-04-30 16:31:17 -0600

    More tests for bend_kick updater.

--- tests/test_bendkick_updater.py
+++ tests/test_bendkick_updater.py
@@ -5,7 +5,7 @@ import numpy as np
 class test_bendkickupdater(object):
 
     def setup(self):
-        self.ensemble = coldatoms.Ensemble(num_ptcls=1)
+        self.ensemble = coldatoms.Ensemble(num_ptcls=3)
         self.ensemble.v[0] = np.array([1.0, 0.0, 0.0])
         self.ensemble.ensemble_properties['charge'] = 3.3
         self.ensemble.ensemble_properties['mass'] = 2.7
@@ -51,5 +51,39 @@ class test_bendkickupdater(object):
 
         coldatoms.bend_kick(dt, Bz, self.ensemble, [force])
 
-        assert(np.linalg.norm(force.position - expected_position) < 1.0e-8)
+        assert(np.linalg.norm(force.position[0] - expected_position) < 1.0e-8)
 
+    def test_near_zero_field_ensemble_omegaB(self):
+        Bz = 1.0e-30
+        dt = 1.0
+
+        self.ensemble.particle_properties['charge'] = np.ones(self.ensemble.v.shape[0])
+        self.ensemble.particle_properties['mass'] = np.ones(self.ensemble.v.shape[0])
+        self.ensemble.ensemble_properties = {}
+
+        expected_position =  self.ensemble.x[0] + dt * self.ensemble.v[0]
+        expected_velocity = self.ensemble.v[0]
+
+        coldatoms.bend_kick(dt, Bz, self.ensemble, [])
+
+        assert(np.linalg.norm(self.ensemble.x[0] - expected_position) < 1.0e-8)
+        assert(np.linalg.norm(self.ensemble.v[0] - expected_velocity) < 1.0e-8)
+
+    def test_closed_circle_ensemble_omegaB(self):
+        Bz = 1.0
+        q = self.ensemble.ensemble_properties['charge']
+        m = self.ensemble.ensemble_properties['mass']
+        omegaB = Bz * q / m
+        dt = 2.0 * np.pi / omegaB
+
+        self.ensemble.particle_properties['charge'] = q * np.ones(self.ensemble.v.shape[0])
+        self.ensemble.particle_properties['mass'] = m * np.ones(self.ensemble.v.shape[0])
+        self.ensemble.ensemble_properties = {}
+
+        expected_position =  self.ensemble.x[0]
+        expected_velocity = self.ensemble.v[0]
+
+        coldatoms.bend_kick(dt, Bz, self.ensemble, [])
+
+        assert(np.linalg.norm(self.ensemble.x[0] - expected_position) < 1.0e-8)
+        assert(np.linalg.norm(self.ensemble.v[0] - expected_velocity) < 1.0e-8)
