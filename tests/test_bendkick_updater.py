3a44ec412a5a5ded86aa9bf0b5f8f29d7ee596d4
Dominic Meiser <dominic@sketchup.com>
2017-06-04 20:18:09 -0600

    Add spot checks for bend-kick updater.

--- tests/test_bendkick_updater.py
+++ tests/test_bendkick_updater.py
@@ -5,7 +5,7 @@ import numpy as np
 class test_bendkickupdater(object):
 
     def setup(self):
-        self.ensemble = coldatoms.Ensemble(num_ptcls=3)
+        self.ensemble = coldatoms.Ensemble(num_ptcls=5)
         self.ensemble.v[0] = np.array([1.0, 0.0, 0.0])
         self.ensemble.ensemble_properties['charge'] = 3.3
         self.ensemble.ensemble_properties['mass'] = 2.7
@@ -87,3 +87,58 @@ class test_bendkickupdater(object):
 
         assert(np.linalg.norm(self.ensemble.x[0] - expected_position) < 1.0e-8)
         assert(np.linalg.norm(self.ensemble.v[0] - expected_velocity) < 1.0e-8)
+
+    def test_spot_check(self):
+        Bz = 1.7
+        q = self.ensemble.ensemble_properties['charge']
+        m = self.ensemble.ensemble_properties['mass']
+        dt = 1.2e-7
+
+        initial_positions = np.random.random(self.ensemble.x.shape)
+        initial_velocities = np.random.random(self.ensemble.v.shape)
+
+        self.ensemble.x = np.copy(initial_positions)
+        self.ensemble.v = np.copy(initial_velocities)
+
+        coldatoms.bend_kick(dt, Bz, self.ensemble, [])
+        final_positions = np.copy(self.ensemble.x)
+        final_velocities = np.copy(self.ensemble.v)
+
+        # now compute the reference solution
+        self.ensemble.x = np.copy(initial_positions)
+        self.ensemble.v = np.copy(initial_velocities)
+        coldatoms.bend_kick(dt, Bz, self.ensemble, [], reference_impl=True)
+        final_positions_ref = np.copy(self.ensemble.x)
+        final_velocities_ref = np.copy(self.ensemble.v)
+
+        assert(np.linalg.norm(final_positions - final_positions_ref) < 1.0e-6)
+        assert(np.linalg.norm(final_velocities - final_velocities_ref) < 1.0e-6)
+    def test_spot_check_per_particle(self):
+        Bz = 1.7
+        q = self.ensemble.ensemble_properties['charge']
+        m = self.ensemble.ensemble_properties['mass']
+        dt = 1.2e-7
+
+        self.ensemble.particle_properties['charge'] = q * np.ones(self.ensemble.v.shape[0])
+        self.ensemble.particle_properties['mass'] = m * np.ones(self.ensemble.v.shape[0])
+        self.ensemble.ensemble_properties = {}
+
+        initial_positions = np.random.random(self.ensemble.x.shape)
+        initial_velocities = np.random.random(self.ensemble.v.shape)
+
+        self.ensemble.x = np.copy(initial_positions)
+        self.ensemble.v = np.copy(initial_velocities)
+
+        coldatoms.bend_kick(dt, Bz, self.ensemble, [])
+        final_positions = np.copy(self.ensemble.x)
+        final_velocities = np.copy(self.ensemble.v)
+
+        # now compute the reference solution
+        self.ensemble.x = np.copy(initial_positions)
+        self.ensemble.v = np.copy(initial_velocities)
+        coldatoms.bend_kick(dt, Bz, self.ensemble, [], reference_impl=True)
+        final_positions_ref = np.copy(self.ensemble.x)
+        final_velocities_ref = np.copy(self.ensemble.v)
+
+        assert(np.linalg.norm(final_positions - final_positions_ref) < 1.0e-6)
+        assert(np.linalg.norm(final_velocities - final_velocities_ref) < 1.0e-6)
