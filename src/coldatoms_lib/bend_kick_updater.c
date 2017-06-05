820ef6fe63ee8a318d0de36a26b93b5febea158d
Dominic Meiser <dominic@sketchup.com>
2017-06-04 19:33:52 -0600

    Use matrix notation in bend-kick update.
    Both to make things more readable and to improve performance.

--- src/coldatoms_lib/bend_kick_updater.c
+++ src/coldatoms_lib/bend_kick_updater.c
@@ -18,13 +18,20 @@ void bend_kick_update_scalar(double dt, double omegaB,
 	struct Vec3 *pos = (struct Vec3 *)x;
 	struct Vec3 *vel = (struct Vec3 *)v;
 
+	double m_pos[2][2] = {
+		{sinTheta / omegaB, (cosTheta - 1.0) / omegaB},
+		{-(cosTheta - 1.0) / omegaB, sinTheta / omegaB}};
+	double m_vel[2][2] = {
+		{cosTheta, -sinTheta},
+		{sinTheta, cosTheta}};
+
 	for (int i = 0; i < num_ptcls; ++i) {
-		pos[i].x += (sinTheta * vel[i].x + (cosTheta - 1.0) * vel[i].y) / omegaB;
-		pos[i].y += (-(cosTheta - 1.0) * vel[i].x + sinTheta * vel[i].y) / omegaB;
+		pos[i].x += m_pos[0][0] * vel[i].x + m_pos[0][1] * vel[i].y;
+		pos[i].y += m_pos[1][0] * vel[i].x + m_pos[1][1] * vel[i].y;
 		pos[i].z += dt * vel[i].z;
 
-		double vx_tmp = cosTheta * vel[i].x - sinTheta * vel[i].y;
-		double vy_tmp = sinTheta * vel[i].x + cosTheta * vel[i].y;
+		double vx_tmp = m_vel[0][0] * vel[i].x + m_vel[0][1] * vel[i].y;
+		double vy_tmp = m_vel[1][0] * vel[i].x + m_vel[1][1] * vel[i].y;
 		vel[i].x = vx_tmp;
 		vel[i].y = vy_tmp;
 	}
