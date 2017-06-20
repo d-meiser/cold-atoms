300d6b18a3c7be008b7b007706e0452f30924bb2
Dominic Meiser <dmeiser79@gmail.com>
2017-06-19 20:31:55 -0600

    Remove an unused variable.

--- src/coldatoms/particles.py
+++ src/coldatoms/particles.py
@@ -228,7 +228,6 @@ class RadiationPressure(object):
                 (self.gamma / 2.0)**2 /
                 ((self.gamma / 2.0)**2 * (1.0 + 2.0 * s_of_r) +
                  deltas**2))
-        num_ptcls = ensemble.x.shape[0]
 
         # Then we compute the recoil momentum according to momentum diffusion.
         # We assume that each atom undergoes a random walk in 3D momentum space
